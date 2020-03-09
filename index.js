cd86a70c6fc8675dafbb27b6ba68d6aedf2b8ecf
CsabaVK <vaganykarc@outlook.com>
2020-03-09 20:27:15 +0100

    register added

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
+const cookieSession = require('cookie-session');
+const bodyParser = require('body-parser');
+
+// bodyParses is needed to get form post inputs inside a request
+app.use(bodyParser.urlencoded({extended: false}));
 
 // set and use routes
 const mainRoutes = require('./routes/main');
 app.use('/', mainRoutes);
+const accountRoutes = require('./routes/account');
+app.use('/account', accountRoutes);
 
 // set public folder to public
 app.use('/public', express.static('public'));
@@ -14,5 +21,14 @@ app.use('/public', express.static('public'));
 // Set view enginge to ejs and set views folder to views
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+// set cookie session+
+const cookieTime = 30 * 24 * 60 * 60 * 1000; // 30 days in ms
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2'],
+  maxAge: cookieTime,
+  expires: new Date(Date.now() + cookieTime),
+}));
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
---
+++ routes/account.js
@@ -0,0 +1,80 @@
+/* eslint-disable max-len */
+/* eslint-disable new-cap */
+const express = require('express');
+const router = express.Router();
+const pwHash = require('password-hash');
+const syncSql = require('sync-sql');
+
+const sqlData = {
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'okjproject',
+};
+
+router.get('/', (req, res) => {
+  const marketAds = require('../market.example.json');
+  res.render('index', {
+    marketAds: marketAds,
+  });
+});
+
+router.post('/register', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const password2 = req.body.password;
+  const email = req.body.email;
+  const birthdate = req.body.birth_date;
+  const languageselector = 'English';
+  // res.json(req.body);
+  let errorMessage = '';
+  if (checkIfUserExists(email)) {
+    errorMessage += 'The user already exists!<br />';
+  } else {
+    if (email && password && password2) {
+      // https://regexr.com/
+      if ((/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}/g.test(email)) && (/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{3,20}$/g.test(password))) {
+        if (password == password2) {
+          const inserted = syncSql.mysql(sqlData, `INSERT INTO users (username, password, email, birth_date, lang) VALUES('${username}', '${pwHash.generate(password)}', '${email}', '${birthdate}', '${languageselector}')`);
+          // res.json(inserted);
+          if (inserted.success == 'false') {
+            res.render('account/regResult', {
+              errorMessage: 'MySQL error',
+            });
+          } else {
+            res.render('account/regResult', {
+              successMessage: 'Success! You are registered!',
+            });
+          }
+          return;
+        } else {
+          errorMessage += 'The passwords are not the same!<br />';
+        }
+      } else {
+        errorMessage += 'Email or password is wrong! Password needs to be between 3 and 20 characters and must contain atleast an uppercase letter and a number.<br />';
+      }
+    } else {
+      errorMessage += 'You can\'t leave anything empty!<br />';
+    }
+  }
+  res.render('account/regResult', {
+    errorMessage: errorMessage,
+  });
+  return;
+});
+
+router.get('/', (req, res) => {
+  res.render('hirdetesMegtekint');
+});
+
+function checkIfUserExists(userEmail) {
+  const exists = syncSql.mysql(sqlData, 'SELECT email FROM users WHERE email=\'' + userEmail + '\'');
+  if (exists && exists.data && exists.data.rows && exists.data.rows[0] && exists.data.rows[0].email) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+module.exports = router;
